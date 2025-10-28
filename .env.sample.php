f67b8609fc73ad8d46691b9948434c53a5aece45
Paola Shani <paolashani@gmail.com>
2025-10-28 18:23:15 +0200

    Add .env.sample.php

---
+++ .env.sample.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'DB_HOST' => '127.0.0.1',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'smart_home',
+  'DB_USER' => 'root',
+  'DB_PASS' => '',
+  'APP_ENV' => 'dev',
+  'APP_TIMEZONE' => 'Europe/Athens',
+  'SESSION_NAME' => 'smarthome_sess'
+];
